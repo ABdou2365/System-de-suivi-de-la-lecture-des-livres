e1aff7a29fc5feaa08fd2d62a0a4a48bd6758229
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2024-06-06 23:04:41 +0100

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -6,133 +6,134 @@ public class Main {
         User user = new User("Moutawakkil");
 
         while (true) {
-            System.out.println("");
-            System.out.println("");
-            System.out.println("1. afficher les livres disponible");
-            System.out.println("2. Ajouter un livre");
-            System.out.println("3. Supprimer un livre");
-            System.out.println("4. Lire un livre");
-            System.out.println("5. Quitter");
-
-            System.out.print("Choisissez une option : ");
+            displayMenu();
             int option = scanner.nextInt();
             scanner.nextLine(); // Pour consommer la nouvelle ligne après nextInt()
 
             switch (option) {
                 case 1:
-                    for (Book book : user.getBooks()) {
-                        System.out.println(book.getTitle() + " - de " +
-                                book.getAuthor() + " - " +
-                                book.getTotalPages() + " Pages");
-                    }
-                    try {
-                        Thread.sleep(4000);
-                        System.out.println("");
-                        System.out.println("");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Est-ce que vous voulez continuer ?");
-                    System.out.println("1. Oui");
-                    System.out.println("2. Non");
-
-                    int decision = scanner.nextInt();
-                    scanner.nextLine();
-                    if (decision == 2) {
-                        System.out.println("Merci d'avoir utilisé notre système de suivi de la lecture des livres, A la prochaine !");
-                        return;
-                    }
+                    displayBooks(user);
                     break;
 
                 case 2:
-                    System.out.print("Titre du livre à ajouter: ");
-                    String title = scanner.nextLine();
-                    System.out.print("Auteur du livre : ");
-                    String author = scanner.nextLine();
-                    System.out.print("Nombre total de pages : ");
-                    int totalPages = scanner.nextInt();
-                    scanner.nextLine(); // Pour consommer la nouvelle ligne après nextInt()
-                    Book book = new Book(title, author, totalPages);
-                    user.addBook(book);
-                    System.out.println("Livre ajouté avec succès !");
+                    addBook(user, scanner);
                     break;
 
                 case 3:
-                    System.out.print("Titre du livre à supprimer : ");
-                    String bookTitle = scanner.nextLine();
-                    Book bookToRemove = null;
-                    for (Book b : user.getBooks()) {
-                        if (b.getTitle().equals(bookTitle)) {
-                            bookToRemove = b;
-                            break;
-                        }
-                    }
-                    if (bookToRemove != null) {
-                        user.removeBook(bookToRemove);
-                        System.out.println("Livre supprimé avec succès !");
-                    } else {
-                        System.out.println("Livre non trouvé.");
-                    }
+                    removeBook(user, scanner);
                     break;
 
                 case 4:
-                    System.out.println();
-                    int number = 0;
-                    for (Book livre : user.getBooks()) {
-                        number++;
-                        if(livre.getCurrentPage() ==0){
-                            System.out.println(number + ".  " + livre.getTitle() + " - de " +
-                                    livre.getAuthor() + " - " + " Vous n'avez pas encore commencer" +
-                                    " a lire ce livre  ");
-                        }
-                        else {
-                            System.out.println(number + ".  " + livre.getTitle() + " - de " +
-                                    livre.getAuthor() + " - " + " Vous êtes arréter sur la page " +
-                                    livre.getCurrentPage());
-                        }
-                    }
-                    System.out.println("");
-                    System.out.print("Titre du livre à lire : ");
-                    String bookTitleToRead = scanner.nextLine();
-                    Book bookToRead = null;
-                    for (Book b : user.getBooks()) {
-                        if (b.getTitle().equals(bookTitleToRead)) {
-                            bookToRead = b;
-                            break;
-                        }
-                    }
-                    if (bookToRead != null) {
-
-                        if(bookToRead.getCurrentPage() ==0){
-                            System.out.println("1. Commencer la lecture");
-                        }
-                        else{
-                            System.out.println("1. reLire le livre depuis le début");
-                            System.out.println("2. Reprendre depuis votre dérnière lecture - page " + bookToRead.getCurrentPage());
-                        }
-                        System.out.println("");
-                        System.out.print("Choisissez une option : ");
-                        int opt = scanner.nextInt();
-                        scanner.nextLine(); // Pour consommer la nouvelle ligne après nextInt()
-                        switch (opt) {
-                            case 1:
-                                user.readBook(bookToRead, scanner);
-                            case 2:
-                                user.resumeReading(bookToRead, scanner);
-                        }
-
-                    } else {
-                        System.out.println("Livre non trouvé.");
-                    }
+                    readBook(user, scanner);
                     break;
 
                 case 5:
-                    System.out.println("Merci d'avoir utilisé notre système de suivi de la lecture des livres, A la prochaine !");
+                    System.out.println("Merci d'avoir utilisé notre système de suivi de la lecture des livres. À la prochaine !");
                     return;
 
                 default:
                     System.out.println("Option invalide. Veuillez choisir à nouveau.");
             }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("\n1. Afficher les livres disponibles");
+        System.out.println("2. Ajouter un livre");
+        System.out.println("3. Supprimer un livre");
+        System.out.println("4. Lire un livre");
+        System.out.println("5. Quitter");
+        System.out.print("Choisissez une option : ");
+    }
+
+    private static void displayBooks(User user) {
+        System.out.println("\nLivres disponibles:");
+        int number = 0;
+        for (Book book : user.getBooks()) {
+            number++;
+            String status = book.getCurrentPage() == 0 ? "Vous n'avez pas encore commencé à lire ce livre" :
+                    "Vous êtes arrêté à la page " + book.getCurrentPage();
+            System.out.println(number + ". " + book.getTitle() + " - de " + book.getAuthor() + " - " + status);
+        }
+        pause();
+    }
+
+    private static void addBook(User user, Scanner scanner) {
+        System.out.print("Titre du livre à ajouter: ");
+        String title = scanner.nextLine();
+        System.out.print("Auteur du livre : ");
+        String author = scanner.nextLine();
+        System.out.print("Nombre total de pages : ");
+        int totalPages = scanner.nextInt();
+        scanner.nextLine(); // Pour consommer la nouvelle ligne après nextInt()
+        Book book = new Book(title, author, totalPages);
+        user.addBook(book);
+        System.out.println("Livre ajouté avec succès !");
+    }
+
+    private static void removeBook(User user, Scanner scanner) {
+        System.out.print("Titre du livre à supprimer : ");
+        String bookTitle = scanner.nextLine();
+        Book bookToRemove = null;
+        for (Book b : user.getBooks()) {
+            if (b.getTitle().equals(bookTitle)) {
+                bookToRemove = b;
+                break;
+            }
+        }
+        if (bookToRemove != null) {
+            user.removeBook(bookToRemove);
+            System.out.println("Livre supprimé avec succès !");
+        } else {
+            System.out.println("Livre non trouvé.");
+        }
+    }
+
+    private static void readBook(User user, Scanner scanner) {
+        System.out.print("Titre du livre à lire : ");
+        String bookTitleToRead = scanner.nextLine();
+        Book bookToRead = null;
+        for (Book b : user.getBooks()) {
+            if (b.getTitle().equals(bookTitleToRead)) {
+                bookToRead = b;
+                break;
+            }
+        }
+        if (bookToRead != null) {
+            if (bookToRead.getCurrentPage() == 0) {
+                System.out.println("1. Commencer la lecture");
+            } else {
+                System.out.println("1. Relire le livre depuis le début");
+                System.out.println("2. Reprendre depuis votre dernière lecture - page " + bookToRead.getCurrentPage());
+            }
+            System.out.print("Choisissez une option : ");
+            int opt = scanner.nextInt();
+            scanner.nextLine(); // Pour consommer la nouvelle ligne après nextInt()
+            switch (opt) {
+                case 1:
+                    user.readBook(bookToRead, scanner);
+                    break;
+                case 2:
+                    user.resumeReading(bookToRead, scanner);
+                    break;
+                default:
+                    System.out.println("Option invalide. Veuillez choisir à nouveau.");
+            }
+        } else {
+            System.out.println("Livre non trouvé.");
+        }
+    }
+
+    private static void pause() {
+        System.out.println("\nEst-ce que vous voulez continuer ?");
+        System.out.println("1. Oui");
+        System.out.println("2. Non");
+        Scanner scanner = new Scanner(System.in);
+        int decision = scanner.nextInt();
+        scanner.nextLine();
+        if (decision == 2) {
+            System.out.println("Merci d'avoir utilisé notre système de suivi de la lecture des livres. À la prochaine !");
+            System.exit(0);
         }
     }
 }
